d24151656eb28a6ecda06ca46ffc5ba9faa5797d
adityapaul26 <adityapaul2603@gmail.com>
2026-03-23 11:19:14 +0530

    ...

--- spotify-backend/src/controller/auth.controller.js
+++ spotify-backend/src/controller/auth.controller.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs');
 const userModel = require('../models/user.model');
 
+// register user
 const registerUser = async (req, res) => {
 
     const { username, email, password, role = 'user' } = req.body;
@@ -36,4 +37,48 @@ const registerUser = async (req, res) => {
     res.cookie("token", token)
 }
 
-module.exports = { registerUser }
+
+//login user
+
+const loginUser = async (req, res) => {
+
+    const { username, email, password } = req.body;
+
+    const user = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+
+    if (!user) {
+        return res.status(401).json({
+            "message": "Invalid credentials"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+        return res.status(401).json({ "message": "Invalid credentials" })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        "message": "User logged in Successfully",
+        "user": {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+    })
+}
+
+module.exports = { registerUser, loginUser }
--- spotify-backend/src/routes/auth.routes.js
+++ spotify-backend/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser } = require('../controller/auth.controller')
+const { registerUser, loginUser } = require('../controller/auth.controller')
 
 
 router.post('/register', registerUser)
+router.post('/login', loginUser)
 
 module.exports = router
